e405ab49f6a8d21c3d29a31b8c65e9efc5b0aa39
jam-jam200 <jessieoyaks4j@gmail.com>
2022-10-25 21:48:12 +0100

    first animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import NikeCard from "./Components/NikeCard";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: linear-gradient(to right, #fbbe01, #000);
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppContainer>
+        <NikeCard />
+      </AppContainer>
+    </>
   );
 }
 
---
+++ src/Components/NikeCard.jsx
@@ -0,0 +1,141 @@
+import React from "react";
+import styled from "styled-components";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import AirJordan from "../Assets/Images/air-jordan-transparent.png";
+import ShoesDetails from "./ShoesDetails";
+
+const CardWrapper = styled.div`
+  width: 100%;
+  /**This determines how the card rotates or tilts in 3d**/
+  perspective: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CardContainer = styled(motion.div)`
+  margin-top: 2em;
+  width: 285px;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 25px;
+  box-shadow: 0 2px 7px 1px rgba(31, 31, 31, 0.2);
+  background-color: #1d1f21;
+  color: #fff;
+  position: relative;
+  cursor: grab;
+`;
+
+const CircleWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100%;
+  overflow: hidden;
+  border-top-right-radius: 25px;
+`;
+
+const Circle = styled.div`
+  position: absolute;
+  width: 350px;
+  height: 350px;
+  top: -4.2em;
+  right: -10em;
+  z-index: 5;
+  background-color: #fbbe01;
+  border-radius: 50%;
+`;
+
+const TopContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: 1.2;
+  position: relative;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 1em 0px;
+`;
+
+const BottomContainer = styled.div`
+  display: flex;
+  flex: 0.8;
+  padding: 0 1em;
+`;
+
+const NikeText = styled.h1`
+  color: #fff;
+  text-transform: uppercase;
+  margin: 0;
+  z-index: 10;
+  font-size: 76px;
+  font-weight: 900;
+`;
+
+const ShoesWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Shoes = styled(motion.div)`
+  width: auto;
+  height: 190px;
+  z-index: 99;
+  user-select: none;
+  margin-right: 3em;
+  margin-top: 4em;
+  img {
+    width: auto;
+    height: 100%;
+    user-select: none;
+  }
+`;
+
+function NikeCard() {
+  //! allways provide the initial values and it starts from zero
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const rotateX = useTransform(y, [-100, 100], [30, -30]);
+  const rotateY = useTransform(x, [-100, 100], [30, -30]);
+
+  return (
+    <CardWrapper>
+      {/* drag elastic is how much you want it to drag, it's a small variable while the drag constraint doesn't allow the dragging to get out of space */}
+      <CardContainer
+        style={{ x, y, rotateX, rotateY, z: 100 }}
+        drag
+        dragElastic={0.16}
+        dragConstraints={{ top: 0, left: 0, bottom: 0, right: 0 }}
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <TopContainer>
+          <CircleWrapper>
+            <Circle />
+          </CircleWrapper>
+          <ShoesWrapper>
+            <Shoes
+              style={{ x, y, rotateX, rotateY, rotate: "-25deg", z: 1000 }}
+              drag
+              dragElastic={0.12}
+              whileTap={{ cursor: "grabbing" }}
+            >
+              <img src={AirJordan} alt="air jordan" />
+            </Shoes>
+          </ShoesWrapper>
+          <NikeText>nike air</NikeText>
+        </TopContainer>
+        <BottomContainer>
+          <ShoesDetails />
+        </BottomContainer>
+      </CardContainer>
+    </CardWrapper>
+  );
+}
+
+export default NikeCard;
